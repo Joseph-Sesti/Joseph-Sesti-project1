d81b5af65f6072b62fcbee18fa105338f3077a57
Joseph Sesti <sestijoey63@gmail.com>
2019-10-08 18:34:50 +0000

    started project

---
+++ lib/Joseph/Sesti/project1/CLI.rb
@@ -0,0 +1,28 @@
+require "Joseph/Sesti/project1/version"
+
+class CLI
+  
+  def call
+    input = ""
+    
+    while input != "exit"
+      puts "To see the best selling Nintendo 64 games, enter 'N64'."
+      puts "To see the best selling Sony Playstation games, enter 'PS1'."
+      puts "To see the best selling Nintendo Gamecube games, enter 'GCN'."
+      puts "To see the best selling Sony Playstation 2 games, enter 'PS2'."
+      
+      input = gets.strip
+      
+      case input
+      when "N64"
+        list_N64
+      when "PS1"
+        list_PS1
+      when "GCN"
+        list_GCN
+      when "PS2"
+        list_PS2
+      end
+    end
+  end
+end
---
+++ lib/Joseph/Sesti/project1/scraper.rb
@@ -0,0 +1,37 @@
+require "Joseph/Sesti/project1/version"
+require 'open-url'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+  def get_N64_page
+    doc = Nokogiri::HTML(open(http://www.vgchartz.com/article/260911/top-10-best-selling-nintendo-64-games/))
+  end
+  
+  def get_PS1_page
+    doc = Nokogiri::HTML(open(http://www.vgchartz.com/article/260879/top-10-best-selling-playstation-games/))
+  end
+  
+  def get_GCN_page
+    doc = Nokogiri::HTML(open(http://www.vgchartz.com/article/261265/top-10-best-selling-gamecube-games/))
+  end
+  
+  def get_PS2_page
+    doc = Nokogiri::HTML(open(http://www.vgchartz.com/article/261033/top-10-best-selling-playstation-2-games/))
+  end
+  
+  def get_N64_list
+    self.get_N64_page.css(".strong")
+  end
+    
+  def get_PS1_list
+    self.get_PS1_page.css(".strong")
+  end
+  
+  def get_GCN_list
+    self.get_GCN_page.css(".strong")
+  end
+  
+  def get_PS2_list
+    self.get_PS2_page.css(".strong")
+  end
