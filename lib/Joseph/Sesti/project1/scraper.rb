c397c326ba6b82618b9e603fffe57e078319b71a
Joseph Sesti <sestijoey63@gmail.com>
2019-10-10 21:52:59 +0000

    scraped what I needed

--- lib/Joseph/Sesti/project1/scraper.rb
+++ lib/Joseph/Sesti/project1/scraper.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
+  
   def get_N64_page
     doc = Nokogiri::HTML(open("http://www.vgchartz.com/article/260911/top-10-best-selling-nintendo-64-games/"))
   end
@@ -21,18 +22,18 @@ class Scraper
   end
   
   def get_N64_list
-    self.get_N64_page.css("div > p > span > strong")
+    self.get_N64_page.css("div > p > span > strong").text
   end
     
   def get_PS1_list
-    self.get_PS1_page.css("div > p > span > strong")
+    self.get_PS1_page.css("div > p > span > strong").text
   end
   
   def get_GCN_list
-    self.get_GCN_page.css("div > p > span > strong")
+    self.get_GCN_page.css("div > p > span > strong").text
   end
   
   def get_PS2_list
-    self.get_PS2_page.css("div > p > span > strong")
+    self.get_PS2_page.css("div > p > span > strong").text
   end
 end
