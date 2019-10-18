61f2a3730f30f8cd93d7aeccbcff05f802d5f06d
Joseph Sesti <sestijoey63@gmail.com>
2019-10-18 16:01:13 +0000

    Finished project for real this time

--- lib/environment.rb
+++ lib/environment.rb
@@ -6,6 +6,5 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative "./project1/scraper"
-require_relative "./project1/best_selling_games"
 require_relative "./project1/CLI"
 require_relative "./project1/version"
